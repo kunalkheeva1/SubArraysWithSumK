ba02ba1d1d98dbaecf93f6f781d608fc97f47886
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-28 20:47:19 -0700

    added useful comments to for the future references, and now the concept of count is clear.

--- src/SubArraysWithSumK.java
+++ src/SubArraysWithSumK.java
@@ -3,18 +3,20 @@ import java.util.HashMap;
 public class SubArraysWithSumK {
 
     public static int subArrayWithSumK(int[] arr, int n, int k){
+        //map stores sums
         HashMap<Integer, Integer> map = new HashMap<>();
         int count =0, sum =0;
         for(int i=0; i<n; i++){
             sum += arr[i];
-
+            //if finds k, then count ++
             if(sum ==k){
                 count++;
             }
+            // if map contains sum -k then add the frequency of sum-k  in the count
             if(map.containsKey(sum-k)){
                 count += map.get(sum-k);
             }
-
+        //else just put sums in the map, and add one to the value over and over wrt to the repetition
             map.put(sum, map.getOrDefault(sum, 0)+1);
         }
         return count;
