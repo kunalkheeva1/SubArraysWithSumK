16c1ef240eef4d73507a8e9de6a5f7f5b4b28d58
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-28 20:44:42 -0700

    added a method to count the subarray with sum k

--- src/SubArraysWithSumK.java
+++ src/SubArraysWithSumK.java
@@ -1,3 +1,30 @@
+import java.util.HashMap;
+
 public class SubArraysWithSumK {
-    
+
+    public static int subArrayWithSumK(int[] arr, int n, int k){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count =0, sum =0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+
+            if(sum ==k){
+                count++;
+            }
+            if(map.containsKey(sum-k)){
+                count += map.get(sum-k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+        return count;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
